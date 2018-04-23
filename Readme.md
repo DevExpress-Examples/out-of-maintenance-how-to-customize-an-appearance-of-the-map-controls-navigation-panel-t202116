73d3f268cb16b47ad519482a0bab64b9ea6cc9d3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to customize an appearance of the map control's navigation panel
+
+
+<p>To customize a navigation panel, use the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapMapControl_NavigationPanelOptionstopic">MapControl.NavigationPanelOptions</a> property. Specify the required parameters of the <a href="https://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraMapNavigationPanelOptionstopic">NavigationPanelOptions</a> object assigned to the property.</p>
+<p>For example, to make the panel invisible, set the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapNavigationPanelOptions_Visibletopic">NavigationPanelOptions.Visible</a> property to <strong>false</strong>.</p>
+
+<br/>
+
+
---
+++ VB/NavigationPanelOptions/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Namespace NavigationPanelOptions
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        #Region "#NavigationPanelOptions"
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            mapControl1.NavigationPanelOptions.XCoordinatePattern = "{CP}{D}.{M}.{S:4}"
+            mapControl1.NavigationPanelOptions.YCoordinatePattern = "{CP}{D}.{M}.{S:4}"
+            mapControl1.NavigationPanelOptions.ShowKilometersScale = False
+            mapControl1.NavigationPanelOptions.ShowMilesScale = False
+            mapControl1.NavigationPanelOptions.ShowScrollButtons = False
+        End Sub
+        #End Region ' #NavigationPanelOptions
+    End Class
+End Namespace
